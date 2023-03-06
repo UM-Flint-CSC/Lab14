1c922831733cf5dd514bbfefd428cda5c3c54bf6
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-03-06 15:21:25 -0500

    Initial Commit

---
+++ Lab14Functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+/*******************************************************************
+* getInfo                                                          *
+* Gets and validates lottery info from the user and places it in   *
+* reference parameters referencing variables in the main function. *
+*******************************************************************/
+
+// code for implementing the getInfo function goes here
+
+
+
+// factorial
+// This function computes factorials recursively. It is called by computeWays.
+
+// code for implementing the factorial function goes here
+
+
+/******************************************************************
+* computeWays                                                     *
+* Computes and returns the number of different possible sets      *
+* of k numbers that can be chosen from a set of n numbers.        *
+* The formula for this is     k!(n-k)!                            *
+*                             --------                            *
+*                                n!                               *
+*******************************************************************/
+
+// code for implementing the computeWays function goes here
+
+
+
