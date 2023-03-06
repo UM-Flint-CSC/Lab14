d86a4eaf4e781a0f62fee1600d60008929dbb031
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-03-06 15:43:04 -0500

    Initial Commit

---
+++ Test2.cpp
@@ -0,0 +1,19 @@
+// This program finds the probability of winning a "mini" lottery
+// when the user's set of numbers match the set drawn
+// by the lottery organizers. It uses three functions.
+# include <iostream>
+using namespace std;
+// Function prototypes
+void getInfo(int&, int&);
+double computeWays(int, int);
+double factorial(int);
+
+int main()
+{ 
+		int n, k;
+		getInfo(n,k);
+		
+
+    return 0;
+}
+
